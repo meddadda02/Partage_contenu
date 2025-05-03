fe0905b8ec9ce47e63abf36403b8176c8ee2be80
AyaBe24 <bekkaliaya49@gmail.com>
2025-05-03 09:42:13 +0100

    aya

--- models/MessageModels.py
+++ models/MessageModels.py
@@ -1,4 +1,4 @@
-from sqlalchemy import Column, Integer, String, ForeignKey, DateTime
+from sqlalchemy import Column, Integer, String, ForeignKey, DateTime, Boolean
 from sqlalchemy.orm import relationship
 from config import Base
 from datetime import datetime
@@ -11,6 +11,11 @@ class Message(Base):
     receiver_id = Column(Integer, ForeignKey('users.id'), nullable=False)
     content = Column(String, nullable=False)
     timestamp = Column(DateTime, default=datetime.utcnow)
+    
+    # Colonnes pour la suppression douce
+    is_deleted_by_sender = Column(Boolean, default=False)
+    is_deleted_by_receiver = Column(Boolean, default=False)
+    deleted_at = Column(DateTime, nullable=True) 
 
     sender = relationship("User", foreign_keys=[sender_id], back_populates="sent_messages")
     receiver = relationship("User", foreign_keys=[receiver_id], back_populates="received_messages")
